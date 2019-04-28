d81955dc13d133742e80228e3f8f39c770c601c6
yangyizhen <397584638@qq.com>
2019-04-28 13:37:48 +0800

    resources

--- src/main/java/com/bluefireplatform/controller/IndoorInfoUploadController.java
+++ src/main/java/com/bluefireplatform/controller/IndoorInfoUploadController.java
@@ -7,10 +7,11 @@
 /*    */ import javax.servlet.http.HttpServletRequest;
 /*    */ import org.springframework.beans.factory.annotation.Autowired;
 /*    */ import org.springframework.stereotype.Controller;
-/*    */ import org.springframework.web.bind.annotation.RequestMapping;
+/*    */ import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 /*    */ import org.springframework.web.bind.annotation.ResponseBody;
 /*    */ import org.springframework.web.multipart.MultipartFile;
-
+            @CrossOrigin
 /*    */ @Controller
 /*    */ public class IndoorInfoUploadController
 /*    */ {
